a15aef10f6b6645b95eeebeca70557a85bed994f
Michael Hugo <mike@piragua.com>
2008-11-11 08:06:56 -0600

    add new domain and tests

--- grails-app/domain/Book.groovy
+++ grails-app/domain/Book.groovy
@@ -1,5 +1,16 @@
 class Book {
 
     String title
+    Date publishedDate
+    Integer pages
+
+    Author primaryAuthor
+
+    static constraints = {
+        title(nullable:true, blank:false)
+        publishedDate(nullable:true)
+        pages(min:0)
+        primaryAuthor(nullable:true)
+    }
 
 }
--- test/unit/BookTests.groovy
+++ test/unit/BookTests.groovy
@@ -1,6 +1,44 @@
-class BookTests extends GroovyTestCase {
+class BookTests extends grails.test.GrailsUnitTestCase {
 
-    void testSomething() {
+    Book book
 
+    void setUp() {
+        super.setUp()
+
+        // in testing-plugin 0.4 you can just do "mockForConstraintsTests(Book)" instead of these two lines
+        registerMetaClass(Book) 
+        grails.test.MockUtils.prepareForConstraintsTests(Book)
+
+        book = new Book(title:'Office Politics',
+                publishedDate: new Date(),
+                pages: 0,
+                primaryAuthor: new Author())
+        assertTrue book.validate()
     }
+
+    void testTitleConstraint() {
+        book.title = null
+        assertTrue book.validate()
+
+        book.title = ''
+        assertFalse book.validate()
+        assertEquals "blank", book.errors["title"]
+    }
+
+    void testPublishedDateConstraint() {
+        book.title = null
+        assertTrue book.validate()
+    }
+
+    void testPagesConstraint() {
+        book.pages = -2
+        assertFalse book.validate()
+        assertEquals "min", book.errors["pages"]
+    }
+
+    void testPrimaryAuthorConstraint() {
+        book.primaryAuthor = null
+        assertTrue book.validate()
+    }
+
 }
